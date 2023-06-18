981322dde1c607e441fc832bc26977e4efdf647d
anajembaedwin <anajembaedwin@gmail.com>
2023-06-18 01:55:22 +0100

    resolved yarn migration error

--- polybase/config.jsx
+++ polybase/config.jsx
@@ -6,8 +6,12 @@ import { Polybase } from "@polybase/client";
 
 // Namespace setup
 
+// const db = new Polybase({
+//   defaultNamespace: "polybase_test_namespace",
+// });
+
 const db = new Polybase({
-  defaultNamespace: "polybase_test_namespace",
+  defaultNamespace: "pk/0x459b5cd08825bddef2229aa0b36cc4000e055174f4a7e4803fb70d98b44bcf4f038a79c3f9c789520c4a67f1303621f58c1dc013033cba98518d6f91ecf3559a/reccoin-test-database",
 });
 
 export default db;
